e6f470660c1df0e0ea58b08f238bcdad5a703ca5
Lithuania54 <edas.simutis@gmail.com>
2024-02-14 19:55:48 +0200

    dinamiskas

--- Masyvai.cpp
+++ Masyvai.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Studentas
 {
     string vardas, pavarde;
-    int kiekis, egzaminorez, *pazymiai;
+    int egzaminorez, *pazymiai;
     double galutinis, mediana;
 };
 
@@ -23,129 +23,136 @@ bool tinkami(const string &name)
 
 int main()
 {
-    int n, vidurkis, minusiukai = 80, stop;
-    Studentas Masyvas[n];
+    double vidurkis = 0;
+    int minusiukai = 80, stop, kiekis = 0;
+    Studentas Masyvas[1000];
 
-    std::cout << "1. - ranka\n2. - generuoti pazymius\n3. - generuoti ir pazymius ir studentu vardus, pavardes\n4. - baigti darba";
-    std::cin >> stop;
 
-    if (n > 0)
+
+    while (true)
     {
-        while (true)
+        std::cout << "1. - ranka\n2. - generuoti pazymius\n3. - generuoti ir pazymius ir studentu vardus, pavardes\n4. - baigti darba" << endl;
+        std::cin >> stop;
+        if (stop < 4)
         {
-
-            if (stop < 4)
+            if (stop == 1)
             {
-               // cout << "Irasykite mokiniu skaiciu: ";
-                //cin >> n;
-
-                for (int i = 0; i < n; i++)
+                int kelintas = 0;
+                do
                 {
+
                     do
                     {
-                        std::cout << "Iveskite varda: ";
-                        cin >> Masyvas[i].vardas;
-                        if (!tinkami(Masyvas[i].vardas))
+                        std::cout << "Iveskite varda (arba 'done', jei norite baigti): ";
+                        cin >> Masyvas[kelintas].vardas;
+                        if (!tinkami(Masyvas[kelintas].vardas))
                         {
                             std::cout << "Vardas turi buti sudarytas tik is raidziu. Bandykite dar karta." << endl;
                         }
-                    } while (!tinkami(Masyvas[i].vardas));
+                    } while (!tinkami(Masyvas[kelintas].vardas));
+                    if(Masyvas[kelintas].vardas=="done") break;
+
+                    kiekis++;
 
                     do
                     {
                         std::cout << "Iveskite pavarde: ";
-                        cin >> Masyvas[i].pavarde;
-                        if (!tinkami(Masyvas[i].pavarde))
+                        cin >> Masyvas[kelintas].pavarde;
+                        if (!tinkami(Masyvas[kelintas].pavarde))
                         {
                             std::cout << "Pavarde turi buti sudaryta tik is raidziu. Bandykite dar karta." << endl;
                         }
-                    } while (!tinkami(Masyvas[i].pavarde));
+                    } while (!tinkami(Masyvas[kelintas].pavarde));
+
+                    int j = 0;
+                    int pazymiukiekis = 1;
+                    Masyvas[kelintas].pazymiai = new int[pazymiukiekis];
 
                     do
                     {
-                        std::cout << "Iveskite pazymiu kieki: ";
-                        cin >> Masyvas[i].kiekis;
-                        if (Masyvas[i].kiekis == 0)
+                        std::cout << "Iveskite pazymius (kai baigsite, parasykite -1): ";
+                        int x; 
+                        cin >> x;
+                        if(x==-1) break; 
+                        if (x< 0 || x > 10)
                         {
-                            std::cout << "Pazymiu kiekis negali buti nulis. Bandykite dar karta." << endl;
+                            std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
                         }
-                    } while (Masyvas[i].kiekis <= 0);
-
-                    for (int j = 0; j < Masyvas[i].kiekis; j++)
-                    {
-                        do
-                        {
-                            std::cout << "Iveskite pazymius: ";
-                            cin >> Masyvas[i].pazymiai[j];
-                            if (Masyvas[i].pazymiai[j] < 0 || Masyvas[i].pazymiai[j] > 10)
-                            {
-                                std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
+                        else{
+                            Masyvas[kelintas].pazymiai[j]=x;
+                            j++;
+                        }
+                        if(j==pazymiukiekis){
+                            pazymiukiekis +=1; 
+                            int * laikinas = new int[pazymiukiekis];
+                            for(int i=0;i<j; i++){
+                                laikinas[i] = Masyvas[kelintas].pazymiai[i];
                             }
-                        } while (Masyvas[i].pazymiai[j] > 10 || Masyvas[i].pazymiai[j] < 0);
-                    }
+                            delete [] Masyvas[kelintas].pazymiai;
+                            Masyvas[kelintas].pazymiai = laikinas; 
+                        }
+                        
+                    } while (true);
 
                     do
                     {
                         std::cout << "Iveskite egzamino rezultata: ";
 
-                        cin >> Masyvas[i].egzaminorez;
-                        if (Masyvas[i].egzaminorez < 0 || Masyvas[i].egzaminorez > 10)
+                        cin >> Masyvas[kelintas].egzaminorez;
+                        if (Masyvas[kelintas].egzaminorez < 0 || Masyvas[kelintas].egzaminorez > 10)
                         {
                             std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
                         }
-                    } while (Masyvas[i].egzaminorez > 10 || Masyvas[i].egzaminorez < 0);
+                    } while (Masyvas[kelintas].egzaminorez > 10 || Masyvas[kelintas].egzaminorez < 0);
 
-                    vidurkis = accumulate(Masyvas[i].pazymiai, Masyvas[i].pazymiai + Masyvas[i].kiekis, 0) / Masyvas[i].kiekis;
-                    Masyvas[i].galutinis = 0.4 * vidurkis + 0.6 * Masyvas[i].egzaminorez;
+                    vidurkis = 1.0* accumulate(Masyvas[kelintas].pazymiai, Masyvas[kelintas].pazymiai + pazymiukiekis, 0) / pazymiukiekis;
+                    Masyvas[kelintas].galutinis = 0.4 * vidurkis + 0.6 * Masyvas[kelintas].egzaminorez;
 
-                    sort(Masyvas[i].pazymiai, Masyvas[i].pazymiai + Masyvas[i].kiekis);
+                    sort(Masyvas[kelintas].pazymiai, Masyvas[kelintas].pazymiai + pazymiukiekis);
 
-                    if (Masyvas[i].kiekis % 2 == 0)
+                    if (pazymiukiekis % 2 == 0)
                     {
-                        Masyvas[i].mediana = (Masyvas[i].pazymiai[Masyvas[i].kiekis / 2] + Masyvas[i].pazymiai[(Masyvas[i].kiekis / 2) - 1]) / 2;
+                        Masyvas[kelintas].mediana = (Masyvas[kelintas].pazymiai[pazymiukiekis / 2] + Masyvas[kelintas].pazymiai[(pazymiukiekis / 2) - 1]) / 2.0;
                     }
-
                     else
                     {
-                        Masyvas[i].mediana = Masyvas[i].pazymiai[Masyvas[i].kiekis / 2];
+                        Masyvas[kelintas].mediana = Masyvas[kelintas].pazymiai[pazymiukiekis / 2];
                     }
-                }
 
-                std::cout.width(15);
-                std::cout << left << "Pavarde";
-                std::cout.width(15);
-                std::cout << left << "Vardas";
-                std::cout.width(23);
-                std::cout << left << "Galutinis (Vid.)";
-                std::cout.width(23);
-                std::cout << left << "Galutinis (med.)" << endl;
-                for (int i = 0; i < minusiukai; i++)
-                {
-                    std::cout << "-";
-                }
-                std::cout << endl;
-
-                for (int i = 0; i < n; i++)
-                {
-                    std::cout.width(15);
-                    std::cout << left << Masyvas[i].pavarde;
-                    std::cout.width(15);
-                    std::cout << left << Masyvas[i].vardas;
-                    std::cout.width(23);
-                    std::cout << left << fixed << setprecision(2) << Masyvas[i].galutinis;
-                    std::cout.width(23);
-                    std::cout << left << fixed << setprecision(2) << Masyvas[i].mediana << endl;
-                }
+                    kelintas++;
+                } while (Masyvas[kelintas].vardas != "done");
             }
-            else
-            {
-                break;
-            }
+        }
+        else
+        {
+            break;
         }
     }
 
-    else
+    std::cout.width(15);
+    std::cout << left << "Pavarde";
+    std::cout.width(15);
+    std::cout << left << "Vardas";
+    std::cout.width(23);
+    std::cout << left << "Galutinis (Vid.)";
+    std::cout.width(23);
+    std::cout << left << "Galutinis (med.)" << endl;
+    for (int i = 0; i < minusiukai; i++)
     {
-        std::cout << "Nera mokiniu" << endl;
+        std::cout << "-";
     }
+    std::cout << endl;
+
+    for (int i = 0; i < kiekis; i++)
+    {
+        std::cout.width(15);
+        std::cout << left << Masyvas[i].pavarde;
+        std::cout.width(15);
+        std::cout << left << Masyvas[i].vardas;
+        std::cout.width(23);
+        std::cout << left << fixed << setprecision(2) << Masyvas[i].galutinis;
+        std::cout.width(23);
+        std::cout << left << fixed << setprecision(2) << Masyvas[i].mediana << endl;
+    }
+    return 0; 
 }
