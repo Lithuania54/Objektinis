174bc410ce3d64eec8e50ef2ac363a11744c2d29
Lithuania54 <edas.simutis@gmail.com>
2024-02-21 18:00:30 +0200

    bug fix

--- Vektoriai.cpp
+++ Vektoriai.cpp
@@ -36,7 +36,6 @@ string RandomString(int ilgis)
 
 int main()
 {
-    srand(time(0));
     double vidurkis = 0;
     int minusiukai = 80, stop, kiekis = 0;
     vector<Studentas> studentai;
@@ -44,8 +43,10 @@ int main()
     while (true)
     {
         Studentas s;
+
         std::cout << "1. - ranka\n2. - generuoti pazymius\n3. - generuoti ir pazymius ir studentu vardus, pavardes\n4. - baigti darba" << endl;
         std::cin >> stop;
+
         if (stop < 4)
         {
             if (cin.fail())
@@ -57,107 +58,105 @@ int main()
             }
             if (stop == 1)
             {
-                do
+                while (true)
                 {
 
-                    do
-                    {
-                        std::cout << "Iveskite varda (arba 'done', jei norite baigti): ";
-                        cin >> s.vardas;
-                        if (!tinkami(s.vardas))
-                        {
-                            std::cout << "Vardas turi buti sudarytas tik is raidziu. Bandykite dar karta." << endl;
-                            cin.clear();
-                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                        }
-                    } while (!tinkami(s.vardas));
+                    std::cout << "Iveskite varda (arba 'done', jei norite baigti): ";
+                    std::cin >> s.vardas;
                     if (s.vardas == "done")
                     {
-                        cin.clear();
-                        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                         break;
-                    }
-
-                    do
-                    {
-                        std::cout << "Iveskite pavarde: ";
-                        cin >> s.pavarde;
-                        if (!tinkami(s.pavarde))
-                        {
-                            cin.clear();
-                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                            std::cout << "Pavarde turi buti sudaryta tik is raidziu. Bandykite dar karta." << endl;
-                        }
-                    } while (!tinkami(s.pavarde));
-
-                    int j = 0;
-
-                    int counter = 0;
-
-                    do
-                    {
-                        std::cout << "Iveskite pazymius (kai baigsite, parasykite -1): ";
-                        int x;
-                        if (cin >> x && ((x > 0 && x <= 10) || (x == -1 && counter > 0)))
-                        {
-                            if (x == -1)
-                            {
-                                break;
-                            }
-                            s.pazymiai.push_back(x);
-                            counter++;
-                            j++;
-                        }
-                        else
-                        {
-                            cin.clear();
-                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                            std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
-                        }
-                    } while (true);
-
-                    do
-                    {
-                        std::cout << "Iveskite egzamino rezultata: ";
-
-                        if (cin >> s.egzaminorez && (s.egzaminorez > 0 && s.egzaminorez <= 10))
-                        {
-                            if (s.egzaminorez < 0 || s.egzaminorez > 10)
-                            {
-                                cin.clear();
-                                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                                std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            cin.clear();
-                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                            std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
-                        }
-                    } while (true);
-
-                    vidurkis = 1.0 * std::accumulate(s.pazymiai.begin(), s.pazymiai.begin() + j, 0) / j;
-                    s.galutinis = 0.4 * vidurkis + 0.6 * s.egzaminorez;
-
-                    std::sort(s.pazymiai.begin(), s.pazymiai.begin() + j);
-
-                    if (j % 2 == 0)
-                    {
-                        s.mediana = (s.pazymiai[j / 2] + s.pazymiai[(j / 2) - 1]) / 2.0;
                     }
                     else
                     {
-                        s.mediana = s.pazymiai[j / 2];
-                    }
+                        if (!tinkami(s.vardas))
+                        {
+                            std::cout << "Vardas turi buti sudarytas tik is raidziu. Bandykite dar karta." << endl;
+                            std::cin.clear();
+                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        }
 
-                    kiekis++;
-                    studentai.push_back(s);
-                } while (s.vardas != "done");
+                        do
+                        {
+                            std::cout << "Iveskite pavarde: ";
+                            std::cin >> s.pavarde;
+                            if (!tinkami(s.pavarde))
+                            {
+                                std::cin.clear();
+                                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                                std::cout << "Pavarde turi buti sudaryta tik is raidziu. Bandykite dar karta." << endl;
+                            }
+                        } while (!tinkami(s.pavarde));
+
+                        int j = 0;
+
+                        int counter = 0;
+
+                        do
+                        {
+                            std::cout << "Iveskite pazymius (kai baigsite, parasykite -1): ";
+                            int x;
+                            if (std::cin >> x && ((x > 0 && x <= 10) || (x == -1 && counter > 0)))
+                            {
+                                if (x == -1)
+                                {
+                                    break;
+                                }
+                                s.pazymiai.push_back(x);
+                                counter++;
+                                j++;
+                            }
+                            else
+                            {
+                                std::cin.clear();
+                                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                                std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
+                            }
+                        } while (true);
+
+                        do
+                        {
+                            std::cout << "Iveskite egzamino rezultata: ";
+
+                            if (std::cin >> s.egzaminorez && (s.egzaminorez > 0 && s.egzaminorez <= 10))
+                            {
+                                if (s.egzaminorez < 0 || s.egzaminorez > 10)
+                                {
+                                    std::cin.clear();
+                                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                                    std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                std::cin.clear();
+                                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                                std::cout << "Neatitinka desimbales sistemos. Bandykite dar karta." << endl;
+                            }
+                        } while (true);
+
+                        vidurkis = 1.0 * std::accumulate(s.pazymiai.begin(), s.pazymiai.begin() + j, 0) / j;
+                        s.galutinis = 0.4 * vidurkis + 0.6 * s.egzaminorez;
+
+                        std::sort(s.pazymiai.begin(), s.pazymiai.begin() + j);
+
+                        if (j % 2 == 0)
+                        {
+                            s.mediana = (s.pazymiai[j / 2] + s.pazymiai[(j / 2) - 1]) / 2.0;
+                        }
+                        else
+                        {
+                            s.mediana = s.pazymiai[j / 2];
+                        }
+
+                        kiekis++;
+                        studentai.push_back(s);
+                    }
+                }
             }
 
             else if (stop == 2)
@@ -215,6 +214,8 @@ int main()
                 {
                     s.mediana = s.pazymiai[j / 2];
                 }
+
+                studentai.push_back(s);
                 kiekis++;
             }
 
@@ -243,6 +244,7 @@ int main()
                     s.mediana = s.pazymiai[s.pazymiai.size() / 2];
                 }
 
+                studentai.push_back(s);
                 kiekis++;
             }
         }
@@ -254,8 +256,6 @@ int main()
         {
             cout << "Pasirinkite nuo 1 iki 4" << endl;
         }
-        if (stop != 4)
-            studentai.push_back(s);
     }
 
     std::cout.width(15);
